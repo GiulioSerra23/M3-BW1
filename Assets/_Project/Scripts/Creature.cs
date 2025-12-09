2067431304ff2625ad8050b212feab436e32f41a
SirMouray <marroneriic@gmail.com>
2025-12-09 19:25:27 +0100

    Enemy + EnemyFollower

--- Assets/_Project/Scripts/Creature.cs
+++ Assets/_Project/Scripts/Creature.cs
@@ -3,23 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Creature : MonoBehaviour
+
 { 
     [SerializeField] protected string _name;
+
 
     protected LifeController _lifeController;
     protected TopDownMover2D _mover2D;
     protected AnimationParamHandler _animHandler;
 
-    public Creature(string name, LifeController lifeController, TopDownMover2D mover2D, AnimationParamHandler animHandler)
-    {
-        _name = name;
-        _lifeController = lifeController;
-        _mover2D = mover2D;
-        _animHandler = animHandler;
-    }
 
     private void Awake()
     {
+
         _lifeController = GetComponent<LifeController>();
         _mover2D = GetComponent<TopDownMover2D>();
         _animHandler = GetComponent<AnimationParamHandler>();
