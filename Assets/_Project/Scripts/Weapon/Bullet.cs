d2ff3635a358453c691d66f59ba3c1bdb3ae463a
RELLAOW <serragiulio05@gmail.com>
2025-12-11 22:17:11 +0100

    added bullets and weapons sprites

--- Assets/_Project/Scripts/Weapon/Bullet.cs
+++ Assets/_Project/Scripts/Weapon/Bullet.cs
@@ -19,10 +19,7 @@ public class Bullet : MonoBehaviour
     {
         if (_rb == null) _rb = GetComponent<Rigidbody2D>();
         _rb.velocity = direction * _speed;
-<<<<<<< Updated upstream
         transform.up = direction;
-=======
->>>>>>> Stashed changes
     }
 
     protected void OnCollisionEnter2D(Collision2D collision)
